7d6293edb8925b9d70e31e03d3f4152088b3157d
user <user@ED04>
2020-09-29 09:40:19 +0800

    lab 1 p2

---
+++ src/lab1/Main.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class Main{
+	public static void main (String[] args) {
+		System.out.println("I'm Chen Guan Hong");
+		System.out.println("My nid is D0707836");
+		System.out.println("This is lab l hw");
+	}
+}
